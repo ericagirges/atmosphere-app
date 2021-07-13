280a1eb77f86d1caccbd0f5a9d9295a4aea94c64
Erica Girges <erica.girges@zendesk.com>
2021-07-13 16:08:48 -0700

    landing app page styling progress

--- atmosphere/pages/top-bar/index.js
+++ atmosphere/pages/top-bar/index.js
@@ -10,11 +10,19 @@ export async function getServerSideProps({ query }) {
 
 export default function TopBar() {
   return (
-      <div  className='container h-screen flex flex-col justify-center space-y-6 text-center'>
-        <p className='font-sans text-lg italic'>{"What's the weather like today"}</p>
+      <div  className='bg-gradient-to-t from-blue-50 via-blue-200 to-blue-400 container h-screen flex flex-col justify-center space-y-6 text-center'>
+        <div className='text-9xl animate-pulse fixed left-12 top-3'>☁️</div>
+        <div className='text-6xl animate-pulse fixed inset-x-0 top-0'>☁️</div>
+        <div className='text-9xl animate-pulse fixed right-10 top-5'>☁️</div>
+        <div className='text-6xl animate-pulse fixed right-0 top-10'>☁️</div>
+        <p className='font-sans text-lg italic'>{"What's the weather like today?"}</p>
         <Link href="/word-cloud">
         <a>View Forecast</a>
         </Link>
+        <div className='text-7xl animate-pulse fixed inset-y-0.5'>☁️</div>
+        <div className='text-9xl animate-pulse fixed left-4 bottom-12'>☁️</div>
+        <div className='text-9xl animate-pulse fixed inset-x-0 bottom-0'>☁️</div>
+        <div className='text-8xl animate-pulse fixed right-4 bottom-9'>☁️</div>
       </div>
   );
 }
